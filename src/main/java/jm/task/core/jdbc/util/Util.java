032cb79fba47950d2826bc21d708e2980e05d805
BalticLW <revizorovalex@gmail.com>
2025-07-14 21:04:46 +0400

    Go

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,31 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/sys";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "0102030911Sa";
+    private static Connection connection;
+    private static void initializeConnection() {
+        //Statement statement;
+        //ResultSet resultSet;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            //statement = connection.createStatement();
+            //resultSet = statement.executeQuery("select * from users");
+            //while (resultSet.next()) {}
+            if (!connection.isClosed()) {
+                System.out.println("connected");
+            }
+        } catch (SQLException e) {
+            System.err.println(e + "no connetion");
+        }
+    }
+    public static Connection getConnection() {
+        if (connection == null) {
+            initializeConnection();
+        }
+        return connection;
+    }
 }
