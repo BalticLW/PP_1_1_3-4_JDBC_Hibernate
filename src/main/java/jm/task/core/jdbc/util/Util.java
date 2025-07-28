4f535cf25be7b178421c5ba33a2e27aaa0e43d8a
BalticLW <revizorovalex@gmail.com>
2025-07-28 13:57:51 +0400

    Hibernate

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,31 +1,59 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.*;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/sys";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "0102030911Sa";
     private static Connection connection;
+    private static SessionFactory sessionFactory;
+
     private static void initializeConnection() {
-        //Statement statement;
-        //ResultSet resultSet;
         try {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            //statement = connection.createStatement();
-            //resultSet = statement.executeQuery("select * from users");
-            //while (resultSet.next()) {}
-            if (!connection.isClosed()) {
-                System.out.println("connected");
-            }
         } catch (SQLException e) {
-            System.err.println(e + "no connetion");
+            throw new RuntimeException(e + ". Ошибка соединения с базой данных!");
         }
     }
+
     public static Connection getConnection() {
         if (connection == null) {
             initializeConnection();
         }
         return connection;
     }
+
+    private static void initializeSessionFactory() {
+        Configuration configuration = new Configuration();
+        Properties settings = new Properties();
+        settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        settings.put(Environment.URL, URL);
+        settings.put(Environment.USER, USERNAME);
+        settings.put(Environment.PASS, PASSWORD);
+        settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+        settings.put(Environment.SHOW_SQL, "true");
+        settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        configuration.setProperties(settings);
+        configuration.addAnnotatedClass(User.class);
+
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            initializeSessionFactory();
+        }
+        return sessionFactory;
+    }
+
 }
